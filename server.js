f095d004acdfa2e2238b9ceaf9d4574f529f5611
Priya Iyar <Priya Iyar>
2017-05-29 16:23:21 -0400

    deleted code

--- server.js
+++ server.js
@@ -32,14 +32,3 @@ db.sequelize.sync({}).then(function() {
 });
 
 // =====================================================
-//Home Page JS
-
-
-  //   // Open
-  // $('.collapsible').collapsible('open', 0);
-
-  // // Close
-  // $('.collapsible').collapsible('close', 0);
-
-  // // Destroy
-  // $('.collapsible').collapsible('destroy');
