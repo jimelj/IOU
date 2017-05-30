278c8e90896d841c1df45a270ab899df760b22e5
Priya Iyar <Priya Iyar>
2017-05-30 19:10:00 -0400

    modal with form

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,14 +1,18 @@
 //sliding function for project table (home page)
-$('.collapsible').collapsible('open', 0);
+// $('.collapsible').collapsible('open', 0);
 
-// Close
-$('.collapsible').collapsible('close', 0);
-// Destroy
-$('.collapsible').collapsible('destroy');
+// // Close
+// $('.collapsible').collapsible('close', 0);
+// // Destroy
+// $('.collapsible').collapsible('destroy');
 
 
 //add project functionality for icon (home page)
 
-$("#addProject").click(function() {
-       console.log("The paragraph was clicked.");
-});
+  
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
+      
