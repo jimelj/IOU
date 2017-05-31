25b34c102989e92047d51796fd482f14fa02db89
Priya Iyar <Priya Iyar>
2017-05-31 11:30:40 -0400

    handlebars for table

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -5,7 +5,19 @@ const db = require('../models');
 
 
 router.get('/', function(req, res) {
-  res.render('home');
+
+	var hi = [{
+
+		project_name: "Test",
+		description: "idk",
+		due_date: "2/3/17",
+		hourly_rate: "15",
+		client_name: "Wonder Woman",
+		client_address: "123adsd",
+		client_email:"asdad"
+		}]
+
+  res.render('home', {project: hi});
 
 //DO NOT TOUCH THIS!!!!
 //   db.Projects.findAll({}).then(function(dbProject) {
